10a7a6919f06a19271b8945419bec62671914259
George Ruzek <gruzek@gmail.com>
2016-04-03 22:05:32 -0400

    Completed first tutorial

--- SpaceInvaders/StartGameScene.swift
+++ SpaceInvaders/StartGameScene.swift
@@ -17,4 +17,16 @@ class StartGameScene: SKScene {
         startGameButton.name = "startgame"
         addChild(startGameButton)
     }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first
+        let touchLocation = touch!.locationInNode(self)
+        let touchedNode = self.nodeAtPoint(touchLocation)
+        if(touchedNode.name == "startgame"){
+            let gameScene = GameScene(size: size)
+            gameScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontalWithDuration(1.0)
+            view?.presentScene(gameScene,transition: transitionType)
+        }
+    }
 }
